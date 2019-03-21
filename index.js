b6c20b26f7aa43e9d2b0df8985ced79574fc0bd8
AaronBruce <aaron.bruce@comcast.net>
2019-03-21 10:18:52 -0700

    refined reader.promises.js and added promise.all() version

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fileReader = require('./lib/reader.js');
 // Obtain and assert input
 let files = process.argv.slice(2);
 
+//instanceof Array  can also be written as  Array.isArray()
 if( ! (files instanceof Array && files.length) ) {
   throw new Error('Invalid Args');
 }
